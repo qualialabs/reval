3923ae950abc84059452f79401f382a6fd9df14d
Jonathan Apodaca <jrapodaca@gmail.com>
2022-04-12 17:37:16 -0600

    Refactor to compute paths correctly

--- plugins/neovim/lua/reval.lua
+++ plugins/neovim/lua/reval.lua
@@ -1,4 +1,6 @@
 local curl = require'plenary.curl'
+local Job = require'plenary.job'
+local Path = require'plenary.path'
 
 M = {}
 
@@ -20,10 +22,31 @@ function M.reval(host)
   end
   last_host = host
 
-  local txt = buftxt()
-  local url = "http://" .. host .. "/reval/reload?filePath=" .. vim.fn.expand('%:p')
-  print(url)
-  curl.post(url, { raw_body = txt })
+  local file = Path:new(vim.fn.expand('%:p'))
+  local repo_root = table.concat(Job:new{
+    command = 'git',
+    args = {'rev-parse', '--show-toplevel'},
+    cwd = tostring(file:parent()),
+    enabled_recording = true,
+  }:sync(), '')
+  local file_relative_to_repo_root = file:make_relative(repo_root)
+  local file_relative_to_dev = '/src/qualia/' .. file_relative_to_repo_root
+
+  local url = "http://" .. host .. "/reval/reload?filePath=/" .. file_relative_to_dev
+  curl.post(url, {
+    raw_body = buftxt(),
+    callback = vim.schedule_wrap(function(result)
+      -- result = {
+      --   body = "",
+      --   exit = 0,
+      --   headers = { "Server: nginx/1.15.12", "Date: Tue, 12 Apr 2022 23:32:30 GMT", "Content-Length: 0", "Connection: keep-alive", "x-content-type-options: nosniff", "", "" },
+      --   status = 200
+      -- }
+      assert(result.exit == 0, "curl exited with a non-zero exit code:" .. result.exit)
+      assert(result.status == 200, "response returned non-200 status-code:" .. result.status)
+      print("done", file_relative_to_dev)
+    end)
+  })
 end
 
 return M
