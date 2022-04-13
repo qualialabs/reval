d46f665c7128267cafb3a9b82f8232b23e2197e5
Jonathan Apodaca <jrapodaca@gmail.com>
2022-04-13 13:23:37 -0600

    Default to host found in .revalrc, if found
    This commit will, upon reval'ing $file, will look "up" for an associated
    ".revalrc" and read it, so that the host argument is not required in those cases

--- plugins/neovim/README.md
+++ plugins/neovim/README.md
@@ -18,7 +18,10 @@ make link
 ## Use
 
 Begin by editing a given file that you will eventually want to `reval`. Once the
-edits are made, simply run `:Reval` (saving the file is not needed). If you need
-to send the reval request to a host other than `localhost:3000`, simply pass
-that argument to the command: `:Reval localhost:4000`. Subsequent uses of the
-`:Reval` invocation will use the same host.
+edits are made, simply run `:Reval` (saving the file is not needed). The plugin
+will search up for a `.revalrc` file that contains the reval host for it to send
+the updated file contents to. If no `.revalrc` file is found, the default host
+of `localhost:3000` is used. If you need to send the reval request to a host
+other than `localhost:3000`, simply pass that argument to the command: `:Reval localhost:4000`.
+If no `.revalrc` file is present, subsequent uses of the `:Reval` invocation
+will use the same host.
--- plugins/neovim/lua/reval.lua
+++ plugins/neovim/lua/reval.lua
@@ -16,13 +16,32 @@ local function buftxt()
   return s
 end
 
+local function find_up(path, file_to_find)
+  local current_directory = Path:new(path)
+  while true do
+    current_directory = current_directory:parent()
+    if current_directory == nil then
+      break
+    end
+
+    local potential_file = current_directory:joinpath(file_to_find)
+    if potential_file:exists() then
+      return Path:new(potential_file:absolute())
+    end
+  end
+end
+
 function M.reval(host)
-  if host == nil or host == "" then
-    host = last_host
+  local file = Path:new(vim.fn.expand('%:p'))
+  local revalrc = find_up(tostring(file), ".revalrc")
+
+  if (host == nil or host == "") and revalrc:exists() then
+    host = revalrc:read()
+  elseif host == nil or host == "" then
+      host = last_host
   end
   last_host = host
 
-  local file = Path:new(vim.fn.expand('%:p'))
   local repo_root = table.concat(Job:new{
     command = 'git',
     args = {'rev-parse', '--show-toplevel'},
@@ -35,6 +54,7 @@ function M.reval(host)
   local url = "http://" .. host .. "/reval/reload?filePath=" .. file_relative_to_dev
   curl.post(url, {
     raw_body = buftxt(),
+    raw = { "-m", "5" },
     callback = vim.schedule_wrap(function(result)
       -- result = {
       --   body = "",
@@ -44,7 +64,7 @@ function M.reval(host)
       -- }
       assert(result.exit == 0, "curl exited with a non-zero exit code:" .. result.exit)
       assert(result.status == 200, "response returned non-200 status-code:" .. result.status)
-      print("done", file_relative_to_dev)
+      print("reval'd", file_relative_to_dev, "at", host)
     end)
   })
 end
